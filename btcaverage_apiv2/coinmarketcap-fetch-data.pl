0c97c7fea9a247d33390935ed87416466beb5676
Gustaf Erikson <gerikson@gmail.com>
2017-06-24 11:25:33 +0200

    get all coins

--- btcaverage_apiv2/coinmarketcap-fetch-data.pl
+++ btcaverage_apiv2/coinmarketcap-fetch-data.pl
@@ -7,9 +7,10 @@ use List::Util qw/max/;
 #use LWP::UserAgent;
 use BTCtracker qw/get_dbh get_ua/;
 my $debug = 0;
-my $url   = 'https://api.coinmarketcap.com/v1/ticker/?limit=10';
+#my $url   = 'https://api.coinmarketcap.com/v1/ticker/?limit=10';
+my $url   = 'https://api.coinmarketcap.com/v1/ticker/';
 #my $url   = 'https://api.coinmarketcap.com/v1/ticker/';
-my $sql   = "insert into coinmarketcap (timestamp, data)
+my $sql = "insert into coinmarketcap (timestamp, data)
 values (datetime(?,'unixepoch'),?)";
 my $ua = get_ua();
 
@@ -23,49 +24,61 @@ if ( !$response->is_success ) {
 
     my $out;
     my @times;
-    my $el_count   = 1;
-    my $coin_count = 0;
-    my $sum_other  = 0;
-    my %changes;
+    my $el_count =1;
+    my $total_count = 0;
+    my $ok_count    = 0;
+    my $sum_other   = 0;
+    my $sum_top     = 0;
+    my %averages = ( available_supply   => 0,
+                     total_supply       => 0,
+                     percent_change_1h  => 0,
+                     percent_change_24h => 0,
+                     percent_change_7d  => 0 );
+    my $others_ref = { name => 'Others', symbol => 'others', id => 'others' };
+
     for my $el ( @{$info} ) {
-#        if ( $el_count <= 10 ) {
+        if ( $el_count <= 10 ) {
             push @{$out}, $el;
             push @times, $el->{last_updated};
-  #      } else {
-#	    warn "==> $el->{name}\n";
-   #          $coin_count++;
+            $sum_top += $el->{market_cap_usd};
+        } else {
+	    
+            $total_count++;
+            next unless defined( $el->{market_cap_usd} );
 
- #            $sum_other += $el->{market_cap_usd}? $el->{market_cap_usd} : 0;
- # 	    foreach my $tag ('1h','24h','7d') {
- # 		next unless ( exists $el->{'percent_change_'.$tag} );
- # 		$changes{$tag} +=
- # 		  ( $el->{'percent_change_'.$tag}?
- # 		    $el->{'percent_change_'.$tag} : 0 ) *
- # 		      $el->{market_cap_usd} ? $el->{market_cap_usd} : 0;
- # 	    }
- #        }
- # # 
-#    $el_count++;
-	}
-    
-    # my $others_ref = {symbol=>$coin_count,rank=>999, name=>'Others',id=>'others',
-    # 		      market_cap_usd=>$sum_other, };
-    # for my $tag ( '1h','24h','7d') {
-    # 	$others_ref->{'percent_change_'.$tag} = $changes{$tag}/$sum_other;
-    # }
-    # $others_ref->{available_supply} = -1;
-    # $others_ref->{total_supply} = -1;
+            $ok_count++;
+            $sum_other += $el->{market_cap_usd};
 
-#    push @{$out}, $others_ref;
+            # volume weighted average values
+            foreach my $tag ( keys %averages ) {
+                next unless ( defined $el->{$tag} );
+                $averages{$tag} += $el->{$tag} * $el->{market_cap_usd};
+            }
+
+        }
+        $el_count++;
+    }
 
     my $latest = max @times;
+    $others_ref->{rank}           = $total_count;
+    $others_ref->{market_cap_usd} = $sum_other;
+    foreach my $tag ( keys %averages ) {
+        $others_ref->{$tag} = $averages{$tag} / $sum_other;
+    }
+
+    push @{$out}, $others_ref;
+    push @{$out},
+        { latest             => $latest,
+          total_mcap         => $sum_top + $sum_other,
+          total_other_coins  => $total_count,
+          active_other_coins => $ok_count };
 
     if ($debug) {
         print to_json( $out, { ascii => 1, pretty => 1 } ) if $debug;
     } else {
         my $dbh = get_dbh();
         my $sth = $dbh->prepare($sql);
-        $sth->execute( $latest, to_json( $info, { ascii => 1 } ) );
+        $sth->execute( $latest, to_json( $out, { ascii => 1 } ) );
         $sth->finish;
         $dbh->disconnect;
     }
